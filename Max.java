36e76c2f03e0119888eb2c0fb14159e655b60895
akisha <akiliza@yandex.ru>
2016-09-15 11:02:18 +0300

    3.0

--- Max.java
+++ Max.java
@@ -1,18 +1,18 @@
 public class Max {
     public static void main(String[] args) {
         int[] arr = {-2, -5, -7, -1};
-        int Max = arr [0];
-        for (int i = 0; i < arr.length; i++) {
+        int max = arr [0];
+        for (int i = 1; i < arr.length; i++) {
 
-            if (arr[i] > Max) Max = arr[i];
+            if (arr[i] > max) max = arr[i];
         }
         System.out.print("Максимальный элемент массива arr = {");
-        for (int i = 1; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             System.out.print(arr[i]);
             System.out.print(", ");
         }
         System.out.print(arr[arr.length - 1]);
         System.out.print("} равен ");
-        System.out.println(Max);
+        System.out.println(max);
     }
 }
