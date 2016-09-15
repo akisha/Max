383f4908a2749a6b9b6a4821b0cf16717e0c5b17
akisha <akiliza@yandex.ru>
2016-09-15 10:48:42 +0300

    2.0

--- Max.java
+++ Max.java
@@ -1,7 +1,7 @@
 public class Max {
     public static void main(String[] args) {
         int[] arr = {-2, -5, -7, -1};
-        int Max = 0;
+        int Max = arr [0];
         for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] > Max) Max = arr[i];
